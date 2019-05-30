b2cc3bd09dec342efcdd20ff1f914e0c0fe952eb
Phillip Valdez <Phillip0150@gmail.com>
2019-05-30 00:28:24 -0500

    Added
    Added some mp3 sounds, some style, and bootstrap to the game.

--- javascript/game.js
+++ javascript/game.js
@@ -1,61 +1,122 @@
-var alphabet = ("abcdefghijklmnopqrstuvwxyz").split("");
-var randomNumber = Math.floor(Math.random() * alphabet.length);
-var theRandomLetter = alphabet[randomNumber];
+
+var wordDic = ["adonising",
+    "adonize",
+    "adonized",
+    "adonizes",
+    "adonizing",
+    "chiton",
+    "chitons",
+    "chitosan",
+    "chitosans",
+    "chits",
+    "chittagong",
+    "chittagongs",
+    "chitted",
+    "chitter",
+    'detrition',
+    "detritions",
+    "detritovore",
+    "detritovores",
+    "detritus",
+    "detrude",
+    "detruded",
+    "detrudes",
+    "gheraoed",
+    "gheraoes",
+    "gheraoing",
+    "gheraos",
+    "gherkin",
+    "gherkins",
+    "hilloaing",
+    "hilloas",
+    "hillock",
+    "hillocked",
+    "hillocks",
+    "irrepleviable",
+    "irreplevisable",
+    "irreprehensible",
+    "irreprehensibly",
+
+]
+
+//Here I am creating a random number based on the range of the dic.
+var randomNumber = Math.floor(Math.random() * wordDic.length);
+console.log("The word is: " + wordDic[randomNumber]);
+//Here i am using that random number to select a word from the array
+ 
+var theRandomWord = wordDic[randomNumber].split("");
+var theBlankWord = [];
 var wins = 0;
 var losses = 0;
-var numberOfGuesses = 10;
-var guesses = [];
+var letterGuesses = [];
+var numberOfGuesses = theRandomWord.length;
+var img = document.getElementById('blinking_image');
+var loseText = document.getElementById('hiddenLose');
 
+for (var i = 0; i < theRandomWord.length; i++){
+    theBlankWord.push("__");
+}
 
-console.log("Random letter is: " + theRandomLetter);
+function updateDisplay(){
+    document.getElementById("wins").innerHTML = "Wins: " + wins;
+    document.getElementById("losses").innerHTML = "Losses: " + losses;
+    document.getElementById("left").innerHTML = "Guesses left: " + numberOfGuesses;
+    document.getElementById("guesses").innerHTML = 'Your guesses so far: ' + letterGuesses;
+    document.getElementById("word").innerHTML = 'The Word: ' + theBlankWord.join(" ");
+}
 
+updateDisplay();
 document.addEventListener('keydown', function(event) {
+    
+    
     var x = event.key;
-    console.log(x);
-    if(numberOfGuesses > 0) {
-        if (x == theRandomLetter) {
-            ++wins;
-            guesses = "";
-            numberOfGuesses = 10;
-
-            document.getElementById("wins").innerHTML = "Wins: " + wins;
-            document.getElementById("losses").innerHTML = "Losses: " + losses;
-            document.getElementById("left").innerHTML = "Guesses left: " + numberOfGuesses;
-            document.getElementById("guesses").innerHTML = 'Your guesses so far: ' + guesses;
-
-
+    var theIndex = theRandomWord.indexOf(x);
+    img.style.display = 'none';
+    loseText.style.display = "none";
+    // loseText.style.display = 'none';
+    if (theIndex != -1){
+        for (i = 0; i<theRandomWord.length; i++){
+           if (x == theRandomWord[i]) {
+                document.getElementById('audio1').play();
+                theBlankWord[i] = x
+           }
         }
-        else {
-            guesses.push(x);
-            --numberOfGuesses;
-
-            document.getElementById("wins").innerHTML = "Wins: " + wins;
-            document.getElementById("losses").innerHTML = "Losses: " + losses;
-            document.getElementById("left").innerHTML = "Guesses left: " + numberOfGuesses;
-            document.getElementById("guesses").innerHTML = 'Your guesses so far: ' + guesses;
-
-
-        }
+        theBlankWord[theIndex] = x;
+        updateDisplay();
     }
-    else if(numberOfGuesses == 0){
-      
+    if(numberOfGuesses==0 && theBlankWord.indexOf("__")!=-1) {
         losses++;
-        guesses = [];
-        numberOfGuesses = 10;
+        
+        img.style.display = "block";
+        loseText.style.display ="block";
+        
+        randomNumber = Math.floor(Math.random() * wordDic.length);
+        console.log("The word is: " + wordDic[randomNumber]);
+        theRandomWord = wordDic[randomNumber].split("");
+        theBlankWord = [];
+        letterGuesses = [];
+        numberOfGuesses = theRandomWord.length;
 
-        document.getElementById("wins").innerHTML = "Wins: " + wins;
-        document.getElementById("losses").innerHTML = "Losses: " + losses;
-        document.getElementById("left").innerHTML = "Guesses left: " + numberOfGuesses;
-        document.getElementById("guesses").innerHTML = 'Your guesses so far: ' + guesses;
+        for (var i = 0; i < theRandomWord.length; i++){
+            theBlankWord.push("__");
+        }
+        updateDisplay();
         
     }
-
+    if(theIndex == -1) {
+        letterGuesses.push(x);
+        numberOfGuesses--;
+        document.getElementById('audio2').play();
+        updateDisplay();
+    }
+    
 });
 
-document.getElementById("wins").innerHTML = "Wins: " + wins;
-document.getElementById("losses").innerHTML = "Losses: " + losses;
-document.getElementById("left").innerHTML = "Guesses left: " + numberOfGuesses;
-document.getElementById("guesses").innerHTML = 'Your guesses so far: ' + guesses;
+
+
+
+
+
 
 
 
